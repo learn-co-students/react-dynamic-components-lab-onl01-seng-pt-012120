ea045f8a7b409707cc273c5f41e7463f8119cf6f
vanessuniq <vanessuniq@gmail.com>
2020-09-22 21:46:09 -0400

    Done.

---
+++ build/precache-manifest.7d4794b89982ba5407b5aca24c56a722.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fbb9b09b3c632402567",
+    "url": "/static/css/main.73b8fa40.chunk.css"
+  },
+  {
+    "revision": "1fbb9b09b3c632402567",
+    "url": "/static/js/main.1b88c305.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "9da2d74e3d9888d02a60089827f9133d",
+    "url": "/index.html"
+  }
+];
