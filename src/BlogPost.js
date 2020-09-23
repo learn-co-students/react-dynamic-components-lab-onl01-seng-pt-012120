e601b9a080a92e412f0926d210fbcfe063ed5212
vanessuniq <vanessuniq@gmail.com>
2020-09-22 22:16:53 -0400

    Done.

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -10,26 +10,20 @@ export default class BlogPost extends Component {
             "If you don't understand, ask questions. If you're uncomfortable about asking questions, say you are uncomfortable about asking questions and then ask anyway. It's easy to tell when a question is coming from a good place. Then listen some more. Sometimes people just want to feel heard. Here's to possibilities of friendship and connection and understanding. - Chimamanda Ngozi Adichie"
         ]
 
-        return ( <
-            div id = "blog-post"
-            className = "wrapper" >
-            Just like moons and like suns, < br / >
-            With the certainty of tides, < br / >
-            Just like hopes springing high, < br / >
+        return (
+            <div id = "blog-post" className = "wrapper" >
+            Just like moons and like suns, <br/>
+            With the certainty of tides, <br/>
+            Just like hopes springing high, <br/>
             Still I 'll rise.<br/> -
-            Maya Angelou < br / >
+            Maya Angelou < br/>
 
 
-            <
-            Comment commentText = { comments[0] }
-            /> <
-            Comment commentText = { comments[1] }
-            /> <
-            Comment commentText = { comments[2] }
-            />
+            <Comment commentText = { comments[0] }/> 
+            <Comment commentText = { comments[1] }/> 
+            <Comment commentText = { comments[2] }/>
 
-            <
-            /div>
+            </div>
         );
     }
 }
--- src/Comment.js
+++ src/Comment.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 
 export class Comment extends Component {
     render() {
-        return ( <
-            div className = "comment" > { this.props.commentText } < /div>
+        return (
+            <div className="comment" > { this.props.commentText} </div>
         )
     }
 }
